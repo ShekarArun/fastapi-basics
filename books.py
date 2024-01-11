e0d1c3d08237ff648bf36594b06576cf308b8a68
Arun Shekar <shekar.arun@gmail.com>
2024-01-11 12:39:16 +0530

    feat: Add endpoint to fetch all books and one book

--- books.py
+++ books.py
@@ -2,7 +2,24 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'title': 'Title 1', 'author': 'Author 1', 'category': 'science'},
+    {'title': 'Title 2', 'author': 'Author 2', 'category': 'science'},
+    {'title': 'Title 3', 'author': 'Author 3', 'category': 'history'},
+    {'title': 'Title 4', 'author': 'Author 4', 'category': 'math'},
+    {'title': 'Title 5', 'author': 'Author 5', 'category': 'math'},
+    {'title': 'Title 6', 'author': 'Author 2', 'category': 'math'},
+]
 
-@app.get('/')
-async def first_api():
-    return {'message': 'Hello world!'}
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
+
+
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+    return 'Book with matching title not found'
